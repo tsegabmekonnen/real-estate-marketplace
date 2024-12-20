0f2d0cae5a91e8172eda2e75634d6d23a4d58d0b
tsegabmekonnen <tsegabmekonnen@gmail.com>
2024-12-19 21:14:04 -0600

    section-7: completes contact landlord functionality for listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -13,6 +13,7 @@ import {
   FaParking,
   FaShare,
 } from "react-icons/fa";
+import Contact from "../components/Contact";
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -20,7 +21,7 @@ export default function Listing() {
   const [loading, setloading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
-
+  const [contact, setContact] = useState(false);
   const { currentUser } = useSelector((state) => state.user);
   const params = useParams();
 
@@ -133,6 +134,15 @@ export default function Listing() {
                 {listing.furnished ? "Furnished" : "Unfurnished"}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
+              >
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
