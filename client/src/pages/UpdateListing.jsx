e1ec502ec845dc28d8fc79c25fd5f8d787568530
tsegabmekonnen <tsegabmekonnen@gmail.com>
2024-12-21 20:36:09 -0600

    section-8: completes the search UI with its functionality

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import {
